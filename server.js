5160ec3f03cf87736dfb775faf8206963a414287
unknown <shantanushukla64@gmail.com>
2020-04-21 01:30:46 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
   const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
-  
+  const bodyParser = require('body-parser')
   const indexRouter = require('./routes/index')
-  
+  const authorRouter = require('./routes/author')
+  //const indexRouter = require('./routes/index')
+
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
   app.use(express.static('public'))
-  
+  app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
   const mongoose = require('mongoose')
   mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   const db = mongoose.connection
@@ -21,5 +23,6 @@ if (process.env.NODE_ENV !== 'production') {
   db.once('open', () => console.log('Connected to Mongoose'))
   
   app.use('/', indexRouter)
-  
-  app.listen(process.env.PORT || 3000)
+  app.use('/author', authorRouter)
+
+  app.listen(process.env.PORT || 3000) 
