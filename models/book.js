ff98066877deffc81f15416be748ef3b8ce2dfff
unknown <shantanushukla64@gmail.com>
2020-04-24 00:40:03 +0530

    Books/Index/New

--- models/book.js
+++ models/book.js
@@ -1,38 +1,45 @@
 const mongoose = require('mongoose')
+const path = require('path')
 
 const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String
-    },
-    publishDate:{
-        type: Date,
-        required: true
-    },
-    pageCount: {
-        type: Number,
-        required : true
-    },
-    createdAt: {
-        type: Date,
-        required :true,
-        default : Date.now
-    },
-    coverImageName: {
-        type: String,
-        required : true
-    },
-    author:{
-       type : mongoose.Schema.Types.ObjectId,//this is referecing an another object
-       required: true,
-       ref: 'Author'
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImageName: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
 })
 
-module.exports = mongoose.model('Book' , bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath//because we dont want exports it as default we wanna exports it as a named variable which will be called coverImageBasePath 
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImageName != null) {
+    return path.join('/', coverImageBasePath, this.coverImageName)
+  }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
+module.exports.coverImageBasePath = coverImageBasePath
